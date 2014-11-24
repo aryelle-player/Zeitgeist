f4a5bda445280c4fbe9dc084f4bc9460caf84af0
unknown <playeral@EAS-BEN016T9.it.muohio.edu>
2014-11-23 22:03:27 -0500

    more changes

--- apsmTrends.cpp
+++ apsmTrends.cpp
@@ -2,3 +2,12 @@
 
 #include <array>
 
+void apsmTrends::increaseCount(std::string s, unsigned amount){
+	//Pseudocode
+	hashfunction(s)
+		increase the hash by amount or add s to the hash table
+
+	compare s.second (the amount of times it has been used) to the top10 array
+		if s.second is used more than an element of the top10, swap s and that element.
+
+}
--- apsmTrends.h
+++ apsmTrends.h
@@ -1,8 +1,13 @@
 #include "Trends.h"
 #include <array>
+#include <string>
 
 class apsmTrends : public Trends {
 public:
+	apsmTrends(){
+		std::array<std::pair<std::string, unsigned int>, 10> topTen;
+
+	}
 
 	virtual void increaseCount(std::string s, unsigned amount);
 	virtual std::string getNthPopular(unsigned int n);
