6a1d7aa32bbfcb051947f1e52b5cd8aa726c78a2
unknown <playeral@EAS-BEN016T9.it.muohio.edu>
2014-11-23 21:29:28 -0500

    more changes to apsm files

---
+++ apsmTrends.cpp
@@ -0,0 +1,4 @@
+#include "apsmTrends.h"
+
+#include <array>
+
--- apsmTrends.h
+++ apsmTrends.h
@@ -1,5 +1,11 @@
+#include "Trends.h"
+#include <array>
+
 class apsmTrends : public Trends {
-   
+public:
+
+	virtual void increaseCount(std::string s, unsigned amount);
+	virtual std::string getNthPopular(unsigned int n);
 
 
   };
