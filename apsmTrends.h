6b600ca1b9cde59af456f64844296184f5c674d6
Aryelle <playeral@miamioh.edu>
2014-11-24 21:08:08 -0500

    made progress in apsm.h

--- apsmTrends.h
+++ apsmTrends.h
@@ -1,17 +1,21 @@
 #include "Trends.h"
 #include <array>
-#include <string>
+#include <unordered_map>
 
 class apsmTrends : public Trends {
 public:
-	apsmTrends(){
-		std::array<std::pair<std::string, unsigned int>, 10> topTen;
-
-	}
-
-	virtual void increaseCount(std::string s, unsigned amount);
+	virtual void increaseCount(std::string s, unsigned int amount);
+	virtual unsigned int getCount(std::string s);
 	virtual std::string getNthPopular(unsigned int n);
+	virtual unsigned int numEntries();
 
-
+protected:
+	std::array<std::pair<unsigned int, std::string>, 10> topTen;
+	std::unordered_map<std::pair<std::string, unsigned int>> alphabetical;
+	//i declared these as variables rather than making a constructor
+	//brinkman said we don't need a constructor really
+	//the problem with this is that i don't know how to finish it
+	//if you go into the cpp api and lookup the unordered_map container
+	//you'll be able to see how it's supposed to work
   };
 
