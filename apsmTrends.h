4de76c698e5e3df79dec09ae20bf5b05759af0e4
mccarts3 <scott2@scotts-MacBook-Pro-6.local>
2014-11-24 20:22:47 -0600

    Added an unordered map, swap() function declarations

--- apsmTrends.h
+++ apsmTrends.h
@@ -1,17 +1,19 @@
 #include "Trends.h"
+#include <unordered_map>
 #include <array>
 #include <string>
 
 class apsmTrends : public Trends {
 public:
-	apsmTrends(){
+    apsmTrends(){
 		std::array<std::pair<std::string, unsigned int>, 10> topTen;
-
+        std::unordered_map<std::pair<std::string, unsigned int>> wordTable;
 	}
-
+    
 	virtual void increaseCount(std::string s, unsigned amount);
 	virtual std::string getNthPopular(unsigned int n);
-
-
-  };
+    
+private:
+    virtual void swap(std::pair<std::string, unsigned int>, int);
+};
 
